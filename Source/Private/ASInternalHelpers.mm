297af4b4af4fc856c85d51deeff564025a4ad41e
Go Takagi <15936908+shimastripe@users.noreply.github.com>
2021-09-23 23:53:37 +0900

    Workaround for testing

--- Source/Private/ASInternalHelpers.mm
+++ Source/Private/ASInternalHelpers.mm
@@ -70,9 +70,8 @@ void ASInitializeFrameworkMainThread(void)
     if (ASActivateExperimentalFeature(ASExperimentalLayerDefaults)) {
       // Nop. We will gather default values on-demand in ASDefaultAllowsGroupOpacity and ASDefaultAllowsEdgeAntialiasing
     } else {
-      CALayer *layer = [[[UIView alloc] init] layer];
-      allowsGroupOpacityFromUIKitOrNil = @(layer.allowsGroupOpacity);
-      allowsEdgeAntialiasingFromUIKitOrNil = @(layer.allowsEdgeAntialiasing);
+      allowsGroupOpacityFromUIKitOrNil = @(YES); // Use default values here instead of loading the layer
+      allowsEdgeAntialiasingFromUIKitOrNil = @(NO);
     }
     ASNotifyInitialized();
 #if AS_SIGNPOST_ENABLE
