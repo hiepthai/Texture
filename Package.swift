3fcd4d248dd2b30804bee149472e4b841fa492a8
hiepthai <thaichanhiep@gmail.com>
2023-06-02 15:53:36 +0700

    chore: update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,10 +12,10 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "AsyncDisplayKit", url: "https://github.com/shimastripe/Texture/releases/download/3.1.1/AsyncDisplayKit.xcframework.zip", checksum: "6a5bf5794450f66ef37f79713b715bcfd78f974bcc05705ac9b971690a31614a"),
-        .binaryTarget(name: "libwebp", url: "https://github.com/shimastripe/Texture/releases/download/3.1.1/libwebp.xcframework.zip", checksum: "f4d9113f41e49379c6b89b375ad120bc706ddb69c4a164881aaa55a0dec9f1fd"),
-        .binaryTarget(name: "PINCache", url: "https://github.com/shimastripe/Texture/releases/download/3.1.1/PINCache.xcframework.zip", checksum: "548d5716fc1db88fd9ea9443aba07ea7e0e95df09e3c7f9bae36c4dfa65f8969"),
-        .binaryTarget(name: "PINOperation", url: "https://github.com/shimastripe/Texture/releases/download/3.1.1/PINOperation.xcframework.zip", checksum: "a7b0f724ea701f2bf6230503349b252fb74ed66ce10053e0f63c14771c895c0f"),
-        .binaryTarget(name: "PINRemoteImage", url: "https://github.com/shimastripe/Texture/releases/download/3.1.1/PINRemoteImage.xcframework.zip", checksum: "803046666ce9c73b778a574f9c03398dba677bcc17beadb280bb294b9a127ba3"),
+        .binaryTarget(name: "AsyncDisplayKit", url: "https://github.com/hiepthai/Texture/releases/download/3.1.2/AsyncDisplayKit.xcframework.zip", checksum: "573b3b88df3b7087fbe3ab3605b68c5483f30701157001eae6864582e549e38f"),
+        .binaryTarget(name: "libwebp", url: "https://github.com/hiepthai/Texture/releases/download/3.1.2/libwebp.xcframework.zip", checksum: "d0129bb9639a2ca395d62ec9eba1e0919ef8d7e3f6521d468009f4429fe6cb56"),
+        .binaryTarget(name: "PINCache", url: "https://github.com/hiepthai/Texture/releases/download/3.1.2/PINCache.xcframework.zip", checksum: "b1fe9e0d46138d657c1c9085c24974f3c09d20c6420c222b366d65702c1a2ca4"),
+        .binaryTarget(name: "PINOperation", url: "https://github.com/hiepthai/Texture/releases/download/3.1.2/PINOperation.xcframework.zip", checksum: "2c5ebaa15ceec156143040429ecef0fb4f938d99c58a53aaf2fb3cadc97a82e2"),
+        .binaryTarget(name: "PINRemoteImage", url: "https://github.com/hiepthai/Texture/releases/download/3.1.2/PINRemoteImage.xcframework.zip", checksum: "9249f8db198f87f62537763bb2013a70420b25ec63dede2167a97170f992c361"),
     ]
 )
